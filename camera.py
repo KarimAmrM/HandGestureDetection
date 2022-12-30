46c9d3461c77311280bdacd82411072ab9a89e9f
KarimMutwally <karimamr9@outlook.com>
2022-12-30 22:45:09 +0200

    Update camera.py

--- camera.py
+++ camera.py
@@ -4,14 +4,16 @@ import cv2
 # define a video capture object
 vid = cv2.VideoCapture(0)
 i=0
+c=0
 while(True):
-      
+    c+=1
+    
     # Capture the video frame
     # by frame
     ret, frame = vid.read()
     roi=frame[100:500, 100:500]
         
-        
+    
     cv2.rectangle(frame,(100,100),(500,500),(0,255,0),0)    
     hsv = cv2.cvtColor(roi, cv2.COLOR_BGR2HSV)
     # Display the resulting frame
@@ -20,16 +22,15 @@ while(True):
     cv2.imshow('roi', roi)
 
     #take a photo every 2 seconds and save it
-  
-    while True:
+    print(c)
+    if c%100==0:
         i=i+1
         #save the image
-        cv2.imwrite("Hand"+str(i)+".png", roi)
+        cv2.imwrite("hand"+str(i)+".jpg", roi)
         #convert int to string
         #i=str(i)
-        break
-    cv2.waitKey(2000)
-    
+
+
 
 
     
