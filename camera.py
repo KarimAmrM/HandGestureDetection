c0c5fcb877cdd6e3b0bdf456d776a910ba143506
KarimMutwally <karimamr9@outlook.com>
2023-01-01 00:16:40 +0200

    Add accuracy testing

--- camera.py
+++ camera.py
@@ -1,14 +1,14 @@
 import cv2
-  
+
 # define a video capture object
 vid = cv2.VideoCapture(0)
-i=46
+i=102
 c=0
 while(True):
     c+=1
-    
     # Capture the video frame
     # by frame
+    vid.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
     ret, frame = vid.read()
     roi=frame[100:500, 100:500]
     cv2.rectangle(frame,(90,90),(500,500),(0,255,0),0)    
@@ -18,11 +18,10 @@ while(True):
     #take image from the rectangle 
     cv2.imshow('roi', roi)
     #take a photo every 2 seconds and save it
-    print(c)
     if c%20==0:
         i=i+1
         #save the image
-        cv2.imwrite("own/paper/hand"+str(i)+".jpg", roi)
+        cv2.imwrite("own/perfecto/hand"+str(i)+".jpg", roi)
         #convert int to string
         #i=str(i)
     # the 'q' button is set as the
